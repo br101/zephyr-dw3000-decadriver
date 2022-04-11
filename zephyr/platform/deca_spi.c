dac01b56259e6df8598e7bad30aee0d2a999fbef
Bruno Randolf <br1@einfach.org>
2022-04-11 11:58:59 +0100

    Use spi_cs_control from https://github.com/foldedtoad/dwm3000

--- zephyr/platform/deca_spi.c
+++ zephyr/platform/deca_spi.c
@@ -1,5 +1,10 @@
+/*
+ * Copyright 2015 (c) DecaWave Ltd, Dublin, Ireland.
+ * Copyright 2019 (c) Frederic Mes, RTLOC.
+ * Copyright 2021 (c) Callender-Consulting LLC.
+ */
+
 #include <device.h>
-#include <drivers/gpio.h>
 #include <drivers/spi.h>
 #include <logging/log.h>
 #include <zephyr.h>
@@ -11,8 +16,51 @@
 LOG_MODULE_REGISTER(dw3000_spi);
 
 #define DW_INST DT_INST(0, decawave_dw3000)
+#define DW_SPI	DT_PARENT(DT_INST(0, decawave_dw3000))
 
-static struct spi_dt_spec spi = SPI_DT_SPEC_GET(DW_INST, SPI_WORD_SET(8), 0);
+static const struct device* spi;
+static struct spi_cs_control* cs_ctrl = SPI_CS_CONTROL_PTR_DT(DW_INST, 0);
+static struct spi_config spi_cfgs[2] = {0}; // configs for slow and fast
+static struct spi_config* spi_cfg;
+
+int dw3000_spi_init(void)
+{
+	/* set common SPI config */
+	for (int i = 0; i < ARRAY_SIZE(spi_cfgs); i++) {
+		spi_cfgs[i].cs = cs_ctrl;
+		spi_cfgs[i].operation = SPI_WORD_SET(8);
+	}
+
+	spi_cfgs[0].frequency = 2000000; // slow
+	spi_cfgs[1].frequency = 8000000; // fast
+
+	spi_cfg = &spi_cfgs[0];
+
+	spi = device_get_binding(DT_LABEL(DW_SPI));
+	if (!spi) {
+		LOG_ERR("DW3000 SPI binding failed");
+		return -1;
+	} else {
+		LOG_INF("DW3000 on %s", DT_LABEL(DW_SPI));
+	}
+
+	return 0;
+}
+
+void dw3000_spi_speed_slow(void)
+{
+	spi_cfg = &spi_cfgs[0];
+}
+
+void dw3000_spi_speed_fast(void)
+{
+	spi_cfg = &spi_cfgs[1];
+}
+
+void dw3000_spi_fini(void)
+{
+	// TODO
+}
 
 int writetospiwithcrc(uint16_t headerLength, const uint8_t* headerBuffer,
 					  uint16_t bodyLength, const uint8_t* bodyBuffer,
@@ -37,7 +85,7 @@ int writetospiwithcrc(uint16_t headerLength, const uint8_t* headerBuffer,
 		.count = ARRAY_SIZE(tx_buf),
 	};
 
-	return spi_transceive_dt(&spi, &tx, NULL);
+	return spi_transceive(spi, spi_cfg, &tx, NULL);
 }
 
 int writetospi(uint16_t headerLength, const uint8_t* headerBuffer,
@@ -58,10 +106,10 @@ int writetospi(uint16_t headerLength, const uint8_t* headerBuffer,
 		.count = ARRAY_SIZE(tx_buf),
 	};
 
-	return spi_transceive_dt(&spi, &tx, NULL);
+	return spi_transceive(spi, spi_cfg, &tx, NULL);
 }
 
-int readfromspi(uint16_t headerLength, /*const*/ uint8_t* headerBuffer,
+int readfromspi(uint16_t headerLength, uint8_t* headerBuffer,
 				uint16_t readLength, uint8_t* readBuffer)
 {
 	const struct spi_buf tx_buf = {
@@ -83,21 +131,25 @@ int readfromspi(uint16_t headerLength, /*const*/ uint8_t* headerBuffer,
 		},
 	};
 	const struct spi_buf_set rx = {
-		.buffers = &rx_buf,
+		.buffers = rx_buf,
 		.count = ARRAY_SIZE(rx_buf),
 	};
 
-	return spi_transceive_dt(&spi, &tx, &rx);
-}
+	int ret = spi_transceive(spi, spi_cfg, &tx, &rx);
 
-/* our own functions */
+#if (CONFIG_SOC_NRF52840_QIAA)
+	/*
+	 *  This is a hack to handle the corrupted response frame through the
+	 * nRF52840's SPI3. See this project's issue-log
+	 * (https://github.com/foldedtoad/dwm3000/issues/2) for details. The delay
+	 * value is set in the CMakeList.txt file for this subproject.
+	 */
+	if (ret == 0) {
+		for (volatile int i = 0; i < TX_WAIT_RESP_NRF52840_DELAY; i++) {
+			/* spin */
+		}
+	}
+#endif
 
-void dw3000_spi_speed_low(void)
-{
-	spi.config.frequency = 2000000;
-}
-
-void dw3000_spi_speed_high(void)
-{
-	spi.config.frequency = 8000000;
+	return ret;
 }
