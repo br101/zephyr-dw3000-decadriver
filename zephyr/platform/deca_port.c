954eb50d8537d8e1830dad1f4647425fee3cea5e
Bruno Randolf <br1@einfach.org>
2022-05-24 09:36:52 +0100

    Reformat and remove unused file

--- zephyr/platform/deca_port.c
+++ zephyr/platform/deca_port.c
@@ -7,12 +7,12 @@
 
 /* deca_spi.c */
 int dw3000_writetospiwithcrc(uint16_t headerLength, const uint8_t* headerBuffer,
-					  uint16_t bodyLength, const uint8_t* bodyBuffer,
-					  uint8_t crc8);
+							 uint16_t bodyLength, const uint8_t* bodyBuffer,
+							 uint8_t crc8);
 int dw3000_writetospi(uint16_t headerLength, const uint8_t* headerBuffer,
-			   uint16_t bodyLength, const uint8_t* bodyBuffer);
+					  uint16_t bodyLength, const uint8_t* bodyBuffer);
 int dw3000_readfromspi(uint16_t headerLength, uint8_t* headerBuffer,
-				uint16_t readLength, uint8_t* readBuffer);
+					   uint16_t readLength, uint8_t* readBuffer);
 
 decaIrqStatus_t decamutexon(void)
 {
@@ -36,16 +36,15 @@ void deca_usleep(unsigned long time_us)
 }
 
 static const struct dwt_spi_s dw3000_spi_fct = {
-    .readfromspi = dw3000_readfromspi,
-    .writetospi = dw3000_writetospi,
-    .writetospiwithcrc = dw3000_writetospiwithcrc,
-    .setslowrate = dw3000_spi_speed_slow,
-    .setfastrate = dw3000_spi_speed_fast,
+	.readfromspi = dw3000_readfromspi,
+	.writetospi = dw3000_writetospi,
+	.writetospiwithcrc = dw3000_writetospiwithcrc,
+	.setslowrate = dw3000_spi_speed_slow,
+	.setfastrate = dw3000_spi_speed_fast,
 };
 
-const struct dwt_probe_s dw3000_probe_interf = 
-{
-    .dw = NULL,
-    .spi = (void*)&dw3000_spi_fct,
-    .wakeup_device_with_io = dw3000_wakeup,
+const struct dwt_probe_s dw3000_probe_interf = {
+	.dw = NULL,
+	.spi = (void*)&dw3000_spi_fct,
+	.wakeup_device_with_io = dw3000_wakeup,
 };
--- zephyr/platform/deca_probe_interface.h
+++ zephyr/platform/deca_probe_interface.h
@@ -1,6 +1,7 @@
 /*! ----------------------------------------------------------------------------
  * @file    deca_probe_interface.h
- * @brief   Interface structure. Provides external dependencies required by the driver
+ * @brief   Interface structure. Provides external dependencies required by the
+ * driver
  *
  * @attention
  *
@@ -11,6 +12,7 @@
  */
 #ifndef DECA_PROBE_INTERFACE_H
 #define DECA_PROBE_INTERFACE_H
+
 #include "deca_device_api.h"
 
 extern const struct dwt_probe_s dw3000_probe_interf;
