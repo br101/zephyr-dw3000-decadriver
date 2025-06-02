f8d91438173bd738669003ebcdd09ce99e9b8309
Andrew Nyland <andrewlnyland@gmail.com>
2025-06-02 17:46:08 -0600

    Add updated board files

---
+++ boards/arm/qorvo/decawave_dwm3001cdk/board.cmake
@@ -0,0 +1,6 @@
+board_runner_args(jlink "--device=nRF52833_xxAA" "--speed=4000")
+board_runner_args(pyocd "--target=nrf52833")
+set(OPENOCD_NRF5_SUBFAMILY "nrf52")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
